efbcdb2de9ce296c0dcde9c1fb1a8093c2b76fa6
SLeBron <sophieclebron@gmail.com>
2022-08-30 19:06:47 -0600

    added intern test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,3 +1,18 @@
 test('TODO', () => {
     expect(1 === 1).toBe(true);
 });
+
+const { default: test } = require("node:test");
+const Intern = require("../lib/Intern");
+
+let intern = new Intern(
+    'Selina Kyle', '3', 'skyle@gmail.com', 'University of Denver', 
+);
+
+test('Intern should be created', () => {
+    expect(intern instanceof Intern).toBe(true);
+});
+
+test('getSchool should return University of Denver', () => {
+    expect(intern.getSchool()).toBe('Universtiy of Denver');
+});

62ee0ed5718013ebebead159879330dfdbd04ce5
SLeBron <sophieclebron@gmail.com>
2022-08-30 18:53:11 -0600

    created Engineer test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,3 +1,4 @@
+const { default: test } = require("node:test");
 const Engineer = require("../lib/Engineer");
 
 test('TODO', () => {
@@ -8,3 +9,12 @@ let engineer = new Engineer(
     'Bruce Wayne', 1, 'bwayne@gmail.com', 'github.com/bwayne'
 );
 
+test('Engineer should be created', () => {
+    expect(engineer instanceof Engineer).toBe(true);
+});
+
+test('getGithub should return github.com/bwayne', () => {
+    expect(engineer.getGithub()).toBe('github.com/bwayne');
+});
+
+//do i test get name/id/email/role again or was testing it in employee good enough since that is the parent?

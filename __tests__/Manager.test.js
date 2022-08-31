a8211b4bcf2748f5e4f16cef7cf66aa87d485f90
SLeBron <sophieclebron@gmail.com>
2022-08-30 20:50:52 -0600

    added inquirer prompts

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,3 +1,16 @@
 test('TODO', () => {
     expect(1 === 1).toBe(true);
 });
+
+const { default: test } = require("node:test");
+const Manager = require("../lib/Manager");
+
+let manager = new Manager(
+    'John Blake', '4', 'jblake@gmail.com', '1'
+);
+
+test('Manager should be created', () => {
+    expect(manager instanceof Manager).toBe(true);
+});
+
+//officenumber?
